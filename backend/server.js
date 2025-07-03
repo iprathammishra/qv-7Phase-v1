da562e2b0cd08b1ed61d18e0e15dbcd673a5b9fe
Pratham Mishra <pratham.mishra@qvantel.com>
2025-07-03 15:24:55 +0530

    p5 step1 comp and tested

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,46 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import taskRoutes from './routes/tasks.js';
+import client from 'prom-client';
 
 dotenv.config();
 const app = express();
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status'],
+});
+
+const httpRequestDuration = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'status'],
+  buckets: [0.1, 0.3, 0.5, 1, 1.5, 2, 5],
+})
+
+app.use((req, res, next) => {
+  const end = httpRequestDuration.startTimer();
+  res.on('finish', () => {
+    const route = req.route?.path || req.path;
+    httpRequestCounter.inc({ method: req.method, route, status: res.statusCode });
+    end({ method: req.method, route, status: res.statusCode });
+  });
+  next();
+})
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/tasks', taskRoutes);
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+})
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log('MongoDB connected');
